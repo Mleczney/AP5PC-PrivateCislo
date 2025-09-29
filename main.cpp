709bf207e7049f0936217dc497f17904b8f24209
Mleczney <Mleczney@seznam.cz>
2025-09-29 16:54:12 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+
+#include<iostream>
+
 using namespace std;
 
-class cislo {
+class Cislo {
+  private:
+  int privateNumber = 0;
 
+  public:
+  void setPrivateNumber(int newPrivateNumber) {
+    privateNumber = newPrivateNumber;
+  };
+
+  void printPrivateNumber() {
+    cout << "Zadane private number je: " << privateNumber << endl;
+  }
 };
+
+int main() {
+  Cislo prepisCisla;
+
+  int vklad;
+  cout << "Zadej cislo:" << endl;
+  cin >> vklad;
+
+  prepisCisla.setPrivateNumber(vklad);
+  prepisCisla.printPrivateNumber();
+
+  return 0;
+}
